e114c81ed62c74866c3b10c17f6ff35ed2b218db
said99magnitogorsk <129077310+said99magnitogorsk@users.noreply.github.com>
2025-01-16 18:49:08 +0500

    lab3

---
+++ src/example/myapp/AquariumFish.kt
@@ -0,0 +1,30 @@
+package example.myapp
+
+interface FishColor {
+    val color: String
+}
+
+class Shark: FishAction, FishColor {
+    override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
+
+class Plecostomus (fishColor: FishColor = GoldColor):
+    FishAction by PrintingFishAction("eat algae"),
+    FishColor by fishColor
+
+interface FishAction  {
+    fun eat()
+}
+
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
